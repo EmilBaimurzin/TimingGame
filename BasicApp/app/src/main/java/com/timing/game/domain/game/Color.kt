108c2da6afa6befbf81d3a2a113f381c44893565
EmilBaimurzin <Emilbaimurzin@gmail.com>
2023-07-19 09:59:59 +0300

    done'

--- BasicApp/app/src/main/java/com/timing/game/domain/game/Color.kt
+++
@@ -1,8 +0,0 @@
-package com.timing.game.domain.game
-
-enum class Color {
-    RED,
-    GREEN,
-    BLUE,
-    YELLOW
-}
--- BasicApp/app/src/main/java/com/timing/game/ui/game/CallbackViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.timing.game.ui.game
-
-import androidx.lifecycle.ViewModel
-
-class CallbackViewModel: ViewModel() {
-    var callback: (() -> Unit)? = null
-}
